a7124712ae68ca18bcba338d861f664a4496bc8b
Andy Terwilliger <76022623+andyterwilliger@users.noreply.github.com>
2022-03-04 11:46:09 -0600

    classNames assigned to header elements Classnames assigned to header elements, applied flexbox containers to header, image, and h1. all centered.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -14,7 +14,7 @@ function Navbar() {
     <div className="main_banner sticky-top">
       <nav className="navbar navbar-expand-lg p-3 navbar-dark transparent-nav nav-js">
         <Link className="navbar-brand" to="/">
-          Rebrandly
+          <img src="https://hdwy.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fd0c0ab6a-6f1a-4fdb-b38b-7223aa096fa5%2Flogo.png?table=block&id=1b004435-6c35-4867-8c1d-0ef846c623a4&spaceId=425f0f1f-eb89-41cf-b925-85e46de334af&width=1520&userId=&cache=v2" />
         </Link>
         <button
           className="navbar-toggler"
